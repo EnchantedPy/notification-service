2bf74196ec19316c7840a4795cc1121de3b46fa6
EnchantedPy <infinite.mendum@gmail.com>
2025-06-27 19:04:54 +0300

    refactor code style

--- notification/base.py
+++ notification/base.py
@@ -3,6 +3,9 @@ from models import Email
 
 
 class NotificationSender(Protocol):
-
-	async def send(self, *args, **kwargs) -> None:
+	async def send(
+		self,
+		*args,
+		**kwargs
+	) -> None:
 		...
--- notification/mailer.py
+++ notification/mailer.py
@@ -4,17 +4,19 @@ import aiosmtplib
 from core.config import settings
 from notification.models import Email
 
+
 class EmailSender(NotificationSender):
-	async def send(self, email: Email) -> None:
+    async def send(
+		self,
+		email: Email
+	) -> None:
+        msg = EmailMessage()
+        msg['To'] = email.to
+        msg['From'] = settings.smtp_config.get('username')
+        msg['Subject'] = email.subject
+        msg.set_content(email.body)
 
-		msg = EmailMessage()
-
-		msg['To'] = email.to
-		msg['From'] = settings.smtp_config.get('username')
-		msg['Subject'] = email.subject
-		msg.set_content(email.body)
-
-		await aiosmtplib.send(
+        await aiosmtplib.send(
             msg,
             hostname=settings.smtp_config["hostname"],
             port=settings.smtp_config["port"],
