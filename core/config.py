bd687f30f2efd83f67fec0b3e21dba53e1f930b8
Enchanted <infinite.mendum@gmail.com>
2025-07-30 12:32:36 +0300

    Update config.py

--- core/config.py
+++ core/config.py
@@ -6,7 +6,7 @@ class Settings(BaseModel):
     SMTP configuration for sending notifications.
 
     Dictionary keys:
-      	  - hostname: Union[str, None] - The hostname of the SMTP server (e.g., 'smtp.gmail.com').
+      	 - hostname: Union[str, None] - The hostname of the SMTP server (e.g., 'smtp.gmail.com').
          - port: Union[int, None] - The port to connect to (e.g., 587).
          - start_tls: Union[bool, None] - Whether to use STARTTLS encryption.
          - username: Union[str, None] - SMTP username or email address.
